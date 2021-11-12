de9c2d35dfe4e03b4ae013d9dc8918457c8654d2
bigmozi <annealmeids@gmail.com>
2021-11-12 16:21:43 -0300

    arquivo final

---
+++ exercicios-aula/ex-npm-2/main.js
@@ -0,0 +1,14 @@
+//const { default: axios } = require("axios");
+
+const imagem = document.querySelector('#image');
+
+axios.get("https://dog.ceo/api/breeds/image/random")
+    .then((result) => {
+        console.log(result)
+        imagem.src = result.data.message;
+    })
+
+    .catch((error) => {
+        console.log(error);
+    })
+

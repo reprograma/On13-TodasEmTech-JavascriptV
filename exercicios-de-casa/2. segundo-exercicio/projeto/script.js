197467816e69e5ea20242fa7fdc4bd0a84632f0e
Celina de Souza Marinho <celina.marinho@gmail.com>
2021-12-03 15:25:30 -0300

    limpando o código para entrega de exercícios

--- exercicios-de-casa/2. segundo-exercicio/projeto/script.js
+++ exercicios-de-casa/2. segundo-exercicio/projeto/script.js
@@ -19,24 +19,4 @@
 
 
     
-    //<script src="../node_modules/axios/dist/axios.min.js" type="text/javascript"></script>
 
-   /* imagem.src = result.data.message;
-    imagem.classList.add(".imagem")
-  })
-  .catch((error) => {
-    error = "Try again and see the puppy";
-    console.log(error);
-  });*/
-
-
-
-/*const cuteDogs = document.querySelector('#image_puppy');
-
-axios.get("https://dog.ceo/api/breeds/image/random")
-.then((result) =>{
-    cuteDogs.src = result.data.message;
-})
-.catch((error) =>{
-    console.log(error);
-})*/ 
